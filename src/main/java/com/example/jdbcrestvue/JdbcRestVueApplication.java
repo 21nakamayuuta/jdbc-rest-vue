c4ffaf1a8a4fe51846b395e3b133a549126b5567
nakama <21nakama_yuuta@axiz.co.jp>
2021-06-07 22:02:13 +0900

    first commit

---
+++ src/main/java/com/example/jdbcrestvue/JdbcRestVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcrestvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcRestVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcRestVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdbcrestvue/JdbcRestVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcrestvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcRestVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
